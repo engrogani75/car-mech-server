fc6d384b0745388e879039a2e0643be3f92ff4cf
Woolim Plant <Osman Gani>
2023-11-04 17:10:05 +0600

    the index.js file has been updated

--- index.js
+++ index.js
@@ -33,11 +33,12 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
 
     const sevice = client.db("carMech");
     const seviceCollection = sevice.collection("carService");
+    const bookColection = sevice.collection("book");
 
 
  app.get('/services', async(req, res) =>{
@@ -51,8 +52,31 @@ async function run() {
   const query = {_id: new ObjectId(id)}
   const result = await seviceCollection.findOne(query)
   res.send(result)
+ })
 
-  
+
+
+ app.get('/checkout', async(req, res) =>{
+  const cursor = seviceCollection.find();
+  const result = await cursor.toArray();
+  res.send(result)
+})
+
+app.get('/checkout/:id', async(req, res) =>{
+  const id = req.params.id
+  const query = {_id: new ObjectId(id)}
+  const options = {
+    projection: {title: 1, price: 1, img: 1},
+  };
+  const result = await seviceCollection.findOne(query, options)
+  res.send(result)
+ })
+
+ app.post('/orderBook', async(req, res) =>{
+  const order = req.body;
+  console.log(order);
+  const result = await bookColection.insertOne(order)
+  res.send(result)
  })
 
 
